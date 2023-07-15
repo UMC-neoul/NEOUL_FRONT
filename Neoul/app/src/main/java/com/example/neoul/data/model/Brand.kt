2534c25cf3e4aa915e51fbee28b605a130b2aa4d
Nohsein <si2238@naver.com>
2023-07-15 20:03:14 +0900

    Feat: 카테고리 의류 탭 내용 추가

--- Neoul/app/src/main/java/com/example/neoul/data/model/Brand.kt
+++ Neoul/app/src/main/java/com/example/neoul/data/model/Brand.kt
@@ -1,3 +1,3 @@
 package com.example.neoul.data.model
 
-data class Brand(val brandName:String, val brandContent:String, val brandImg: String)
+data class Brand(val brandName: String, val brandContent: String, val brandImg: String)
