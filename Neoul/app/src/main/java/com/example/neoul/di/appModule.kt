923b321c2cd636bb228810205c5c1b357058848a
hangunhee39 <ghee0831@naver.com>
2023-08-13 00:30:01 +0900

    8/12

--- Neoul/app/src/main/java/com/example/neoul/di/appModule.kt
+++ Neoul/app/src/main/java/com/example/neoul/di/appModule.kt
@@ -7,6 +7,8 @@ import com.example.neoul.data.repository.brand.BrandRepository
 import com.example.neoul.data.repository.brand.DefaultBrandRepository
 import com.example.neoul.data.repository.login.DefultLoginRepository
 import com.example.neoul.data.repository.login.LoginRepository
+import com.example.neoul.data.repository.product.DefaultProductRepository
+import com.example.neoul.data.repository.product.ProductRepository
 import com.example.neoul.data.repository.signup.DefultSignupRepository
 import com.example.neoul.data.repository.signup.SignupRepository
 import com.example.neoul.data.repository.story.DefaultStoryRepository
