08b04068b32d9ca5252dc377ea6c85aeddd95751
Stanley <jk2014jk@naver.com>
2023-08-22 00:21:36 +0900

    feat:마이페이지 오류 수정 완료

--- Neoul/app/src/main/java/com/example/neoul/presentation/main/home/HomeFragment.kt
+++ Neoul/app/src/main/java/com/example/neoul/presentation/main/home/HomeFragment.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 
 
 import androidx.recyclerview.widget.GridLayoutManager
